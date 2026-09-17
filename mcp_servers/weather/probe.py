e09a2e63ba1560b8c743ef95b6b2a00103e31d0e
sanjay-sts <52684316+sanjay-sts@users.noreply.github.com>
2026-09-16 23:11:12 -0600

    httpx working sync

--- mcp_servers/weather/probe.py
+++ mcp_servers/weather/probe.py
@@ -4,7 +4,7 @@ import os
 from pydantic import BaseModel, ValidationError
 from pydantic.functional_validators import field_validator
 from pydantic_extra_types.coordinate import Latitude, Longitude, Coordinate
-
+import httpx
 
 
 load_dotenv()
@@ -13,6 +13,8 @@ openweatherapi = os.getenv('OPENWEATHER_KEY')
 
 class WeatherModel(BaseModel):
     city: str
+    state: str
+    country: str
     desc: str
     temp: float
     @field_validator('temp')
@@ -29,8 +31,10 @@ class WeatherModel(BaseModel):
 
 test_json_data = '''{
     "city":"calgary",
+    "state":"AB",
+    "country":"CA",
     "desc":"calgary city",
-    "temp":67.4,
+    "temp":-1.4,
     "lat": 40.0,
     "long": -87.9
 }
@@ -38,3 +42,38 @@ test_json_data = '''{
 
 user_input = WeatherModel.model_validate_json(test_json_data)
 print(user_input.model_dump_json(indent=2))
+print(user_input.city)
+
+
+
+# # Need to pull from this url
+# # http://api.openweathermap.org/geo/1.0/direct?q={city name},{state code},{country code}&limit={limit}&appid={API key}
+## https://api.openweathermap.org/data/4.0/onecall/current?lat={lat}&lon={lon}&appid={API key}
+
+
+
+base_weather_url = "http://api.openweathermap.org/"
+geo_endpoint = "geo/1.0/"
+data_endpoint = "data/2.5/weather"
+
+direct_query = f"direct?q={user_input.city},{user_input.state},{user_input.country}&limit=5&appid={openweatherapi}"
+full_url_geocoding = base_weather_url+geo_endpoint+direct_query
+print(full_url_geocoding)
+
+response = httpx.get(full_url_geocoding)
+print(response.status_code)
+print(response.content)
+print(response.json())
+lat = response.json()[0].get('lat')
+long = response.json()[0].get('lon')
+
+print(f"lat = {lat}, long = {long}")
+current_temp_query = f"/onecall/current?lat={lat}&{long}"
+
+# full_current_weather = base_weather_url+data_endpoint+current_temp_query
+full_current_weather = f'https://api.openweathermap.org/data/2.5/weather?lat=51.0456064&lon=-114.057541&units=metric&appid={openweatherapi}'
+
+current_weather_response = httpx.get(full_current_weather)
+print(current_weather_response.status_code)
+print(current_weather_response.content)
+print(current_weather_response.json())
